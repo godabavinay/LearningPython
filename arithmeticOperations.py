a3fa7a81e52de36d15acbd151e6b074957432dff
Vinay Godaba <godabavinay@gmail.com>
2019-06-28 20:10:19 +0530

    learning basics

---
+++ arithmeticOperations.py
@@ -0,0 +1,13 @@
+print(2 + 3)
+
+print(45 - 21)
+
+print(34 * 23)
+
+print(2 / 3423)
+
+print(23 % 5)
+
+print(2 ** 3)
+
+print(9 // 3)
---
+++ variablesAndLogicalOperators.py
@@ -0,0 +1,7 @@
+a = 2
+b = 4
+
+if a == 2 and b == 3:
+    print(str(a) + " and " + str(b) + " are 2 and 3")
+else:
+    print("either of " + str(a) + " or " + str(b) + " is not equal to verified values")
